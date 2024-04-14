6e5b1e016156da6dfbb849994caf0a14eec7f175
Kana Fuyuko <141511921+Kana-Fuyuko@users.noreply.github.com>
2024-04-14 06:13:43 -0400

    Update stdafx.h

--- ViewSplit/stdafx.h
+++ ViewSplit/stdafx.h
@@ -1,6 +1,6 @@
-﻿// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
-// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
-// を記述します。
+// stdafx.h : Include file for standard system include files, or
+// Project-specific include files that are frequently referenced and rarely modified
+// Write.
 //
 
 #pragma once
@@ -8,12 +8,12 @@
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーから使用されていない部分を除外します。
-// Windows ヘッダー ファイル:
+#define WIN32_LEAN_AND_MEAN             // Windows Exclude unused parts from the header.
+// Windows header file:
 #include <windows.h>
 
 
-// TODO: プログラムに必要な追加ヘッダーをここで参照してください
+// TODO: See here additional headers required for your program
 #include <d3d9.h>
 #include <DirectXMath.h>
 #include <windowsx.h>
